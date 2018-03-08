98a9921c40eff9e98e723e56f4d0a8ea72e190c4
Tim Long <Tim@tigranetworks.co.uk>
2018-03-08 17:15:40 +0000

    Changed assembly informational version to 0.5.0

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.5.*")]
 [assembly: AssemblyFileVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0-Integration")]
+[assembly: AssemblyInformationalVersion("0.5.0")]
 [assembly: InternalsVisibleTo("TA.Ascom.ReactiveCommunications.Specifications")]
