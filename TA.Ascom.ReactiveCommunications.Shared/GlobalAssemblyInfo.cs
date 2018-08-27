ee138b8903ce80818349d9fb1a0f26daa6052a10
Tim Long <Tim@tigranetworks.co.uk>
2018-08-27 22:07:35 +0100

    Finally chased down a really annoying reference issue with System.ValueTuple, which we are not even using!!

---
+++ TA.Ascom.ReactiveCommunications.Shared/GlobalAssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿// This file is part of the TA.Ascom.ReactiveCommunications project
+// 
+// Copyright © 2018 Tigra Astronomy, all rights reserved.
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so. The Software comes with no warranty of any kind.
+// You make use of the Software entirely at your own risk and assume all liability arising from your use thereof.
+// 
+// File: GlobalAssemblyInfo.cs  Last modified: 2018-08-26@15:27 by Tim Long
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyConfiguration("Uncontrolled build")]
+[assembly: AssemblyCompany("Tigra Astronomy")]
+[assembly: AssemblyProduct("Reactive ASCOM")]
+[assembly: AssemblyCopyright("Copyright © 2015-2017 Tigra Astronomy, all rights reserved")]
+[assembly: AssemblyTrademark("Tigra Astronomy and Tigra Networks are trademarks")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.1.*")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: InternalsVisibleTo("TA.Ascom.ReactiveCommunications.Specifications")]
--- TA.Ascom.ReactiveCommunications.Specifications/SerialChannelSpecs.cs
+++ TA.Ascom.ReactiveCommunications.Specifications/SerialChannelSpecs.cs
@@ -1,22 +1,19 @@
 ﻿// This file is part of the TA.Ascom.ReactiveCommunications project
 // 
-// Copyright © 2016 Tigra Astronomy, all rights reserved.
+// Copyright © 2018 Tigra Astronomy, all rights reserved.
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 // documentation files (the "Software"), to deal in the Software without restriction, including without limitation
 // the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so,. The Software comes with no warranty of any kind.
+// permit persons to whom the Software is furnished to do so. The Software comes with no warranty of any kind.
 // You make use of the Software entirely at your own risk and assume all liability arising from your use thereof.
 // 
-// File: SerialChannelSpecs.cs  Last modified: 2016-10-23@23:36 by Tim Long
+// File: SerialChannelSpecs.cs  Last modified: 2018-08-27@17:51 by Tim Long
 
 using System;
-using System.IO.Ports;
 using FakeItEasy;
 using Machine.Specifications;
 using TA.Ascom.ReactiveCommunications.Specifications.Contexts;
-using TA.Ascom.ReactiveCommunications.Specifications.Fakes;
-using TA.Ascom.ReactiveCommunications.Specifications.Helpers;
 
 // ReSharper disable ComplexConditionExpression
 
@@ -26,41 +23,41 @@ namespace TA.Ascom.ReactiveCommunications.Specifications
     public class when_constructing_a_serial_communication_channel :
         with_serial_channel_on_endpoint_com1_9600_n_8_1_and_mock_serial_port
         {
-        private Because of = () => { };
-        private It should_not_connect_automatically = () => A.CallTo(() => Channel.Port.Open()).MustNotHaveHappened();
-        private It should_save_the_endpoint =
+        Because of = () => { };
+        It should_not_connect_automatically = () => A.CallTo(() => Channel.Port.Open()).MustNotHaveHappened();
+        It should_save_the_endpoint =
             () => Channel.endpoint.ShouldBeTheSameAs(DeviceEndpoint);
         }
 
     [Subject(typeof(SerialCommunicationChannel), "construction")]
     public class when_constructing_a_serial_channel_with_the_wrong_type_of_endpoint
         {
-        private static SerialCommunicationChannel Channel;
-        private static DeviceEndpoint DeviceEndpoint;
-        private static Exception Thrown;
-        private Establish context = () =>
-                DeviceEndpoint = new NetworkDeviceEndpoint("dummy", 8080);
-        private Because of =
+        Establish context = () =>
+            DeviceEndpoint = new NetworkDeviceEndpoint("dummy", 8080);
+        Because of =
             () => Thrown = Catch.Exception(() => Channel = new SerialCommunicationChannel(DeviceEndpoint));
-        private It should_throw_argument_exception = () => Thrown.ShouldBeOfExactType<ArgumentException>();
+        It should_throw_argument_exception = () => Thrown.ShouldBeOfExactType<ArgumentException>();
+        static SerialCommunicationChannel Channel;
+        static DeviceEndpoint DeviceEndpoint;
+        static Exception Thrown;
         }
 
     [Subject(typeof(SerialCommunicationChannel), "Connecting")]
     public class when_the_serial_channel_is_opened :
         with_serial_channel_on_endpoint_com1_9600_n_8_1_and_mock_serial_port
         {
-        private Because of = () => Channel.Open();
-        private It should_configure_the_port_with_the_expected_baud_rate =
+        Because of = () => Channel.Open();
+        It should_configure_the_port_with_the_expected_baud_rate =
             () => Channel.Port.BaudRate.ShouldEqual(SerialEndpoint.BaudRate);
-        private It should_configure_the_port_with_the_expected_data_bits =
+        It should_configure_the_port_with_the_expected_data_bits =
             () => Channel.Port.DataBits.ShouldEqual(SerialEndpoint.DataBits);
-        private It should_configure_the_port_with_the_expected_parity =
+        It should_configure_the_port_with_the_expected_parity =
             () => Channel.Port.Parity.ShouldEqual(SerialEndpoint.Parity);
-        private It should_configure_the_port_with_the_expected_port_name =
+        It should_configure_the_port_with_the_expected_port_name =
             () => Channel.Port.PortName.ShouldEqual(SerialEndpoint.PortName);
-        private It should_configure_the_port_with_the_expected_stop_bits =
+        It should_configure_the_port_with_the_expected_stop_bits =
             () => Channel.Port.StopBits.ShouldEqual(SerialEndpoint.StopBits);
-        private It should_open_the_serial_port =
+        It should_open_the_serial_port =
             () => A.CallTo(() => MockPort.Open()).MustHaveHappenedOnceExactly();
         }
 
@@ -68,8 +65,8 @@ namespace TA.Ascom.ReactiveCommunications.Specifications
     public class when_the_serial_channel_is_closed :
         with_serial_channel_on_endpoint_com1_9600_n_8_1_and_mock_serial_port
         {
-        private Because of = () => Channel.Close();
-        private It should_close_the_serial_port =
+        Because of = () => Channel.Close();
+        It should_close_the_serial_port =
             () => A.CallTo(() => MockPort.Close()).MustHaveHappenedOnceExactly();
         }
 
@@ -77,65 +74,33 @@ namespace TA.Ascom.ReactiveCommunications.Specifications
     public class when_the_serial_channel_is_disposed :
         with_serial_channel_on_endpoint_com1_9600_n_8_1_and_mock_serial_port
         {
-        private Establish context = () => A.CallTo(() => MockPort.IsOpen).Returns(true);
-        private Because of = () =>
+        Establish context = () => A.CallTo(() => MockPort.IsOpen).Returns(true);
+        Because of = () =>
             {
             Channel.Open();
             Channel.Dispose();
             Thrown = Catch.Exception(() => Channel.Dispose());
             };
-        private It should_close_the_serial_port =
+        It should_close_the_serial_port =
             () => A.CallTo(() => MockPort.Close()).MustHaveHappenedOnceExactly();
-        private It should_dispose_the_serial_port =
+        It should_dispose_the_serial_port =
             () => A.CallTo(() => MockPort.Dispose()).MustHaveHappenedOnceExactly();
-        private It should_not_throw_when_disposed_twice = () => Thrown.ShouldBeNull();
+        It should_not_throw_when_disposed_twice = () => Thrown.ShouldBeNull();
         }
 
     [Subject(typeof(SerialCommunicationChannel), "Sending")]
     public class when_a_string_is_sent_to_the_serial_channel :
         with_serial_channel_on_endpoint_com1_9600_n_8_1_and_mock_serial_port
         {
-        private const string TestString = "Test String";
-        private Establish context = () =>
+        Establish context = () =>
             {
             A.CallTo(() => MockPort.IsOpen).Returns(true);
             Channel.Open();
             };
-        private Because of = () => Channel.Send(TestString);
-        private It should_send_the_string_to_the_serial_port = () =>
+        Because of = () => Channel.Send(TestString);
+        It should_send_the_string_to_the_serial_port = () =>
             A.CallTo(() => MockPort.Write(A<string>.That.Matches(s => s == TestString)))
                 .MustHaveHappenedOnceExactly();
-        }
-
-    [Ignore("Upgrading FakeItEasy broke the event handling")]
-    [Subject(typeof(SerialCommunicationChannel), "Loopback test")]
-    internal class when_sending_and_receiving_over_a_serial_channel :
-        with_serial_channel_on_endpoint_com1_9600_n_8_1_and_mock_serial_port
-        {
-        private const string ExpectedResponse = "Pong";
-        private const string Command = "Ping";
-        private static ReactiveTransactionProcessor Processor;
-        private static BufferInputUntilCompletedTransaction Transaction;
-        private static TransactionObserver TransactionObserver;
-        private Establish context = () =>
-            {
-            A.CallTo(() => MockPort.ReadExisting()).Returns(ExpectedResponse);
-            A.CallTo(() => MockPort.Write(A<string>.Ignored)).Invokes(() =>
-                {
-                var eventArgs = MockHelpers.CreateSerialDataReceivedEventArgs(SerialData.Chars);
-                MockPort.DataReceived += Raise.With<SerialDataReceivedEventHandler>(MockPort, eventArgs);
-                });
-            TransactionObserver = new TransactionObserver(Channel);
-            Processor = new ReactiveTransactionProcessor();
-            Processor.SubscribeTransactionObserver(TransactionObserver);
-            Channel.Open();
-            };
-        private Because of = () =>
-            {
-            Transaction = new BufferInputUntilCompletedTransaction(Command, completeAfter: 4);
-            Processor.CommitTransaction(Transaction);
-            Transaction.WaitForCompletionOrTimeout();
-            };
-        private It should_receive_pong = () => Transaction.Value.ShouldEqual(ExpectedResponse);
+        const string TestString = "Test String";
         }
     }
